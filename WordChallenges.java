40660ea9705d540a282c7775c5b48a9ff2fcceb4
spaggers <liz.k.pao@gmail.com>
2023-03-01 09:32:46 -0500

    added word reversal method

--- WordChallenges.java
+++ WordChallenges.java
@@ -1,10 +1,31 @@
+package Challenges1.Challenges;
 
-public class WordChallenges {
-    /**
-     * This is a javadoc comment!
-     */
+/**
+ *
+ */
+public class WordChallenges{
+    private String word;
+
     public static void main(String[] args){
         System.out.print("Hello");
+    }
+
+    public WordChallenges(){
+        this.setWord(" ");
+    }
+
+    public void setWord(String word){
+        this.word = word;
+    }
+
+    public String getWord(){
+        return this.word;
+    }
+
+    public String reverseWord(String word){
+        String[] spliced = word.split(" ");
+        System.out.print(spliced);
+        return word;
     }
 
 }
