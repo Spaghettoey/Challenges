63521d854fbe83e5006c2cb1ed8be599ab626d54
spaggers <liz.k.pao@gmail.com>
2023-03-01 09:47:14 -0500

    added javadoc comments

--- WordChallenges.java
+++ WordChallenges.java
@@ -1,7 +1,9 @@
 package Challenges1.Challenges;
 
-/**
- *
+/** The WordChallenges program contains a collection of methods which alter a string literal.
+ * @author Spaggers
+ * @version 1.0
+ * @since 2/28/23
  */
 public class WordChallenges{
     private String word;
@@ -10,14 +12,23 @@ public class WordChallenges{
         System.out.print("Hello");
     }
 
+    /**
+     *  WordChallenges()
+     */
     public WordChallenges(){
         this.setWord(" ");
     }
 
+    /**
+     * @param word: This is the user-defined word or string-literal
+     */
     public void setWord(String word){
         this.word = word;
     }
 
+    /**
+     * @return  : returns the user-defined word or string-literal
+     */
     public String getWord(){
         return this.word;
     }
